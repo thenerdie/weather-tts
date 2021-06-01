54fdbd963dcef61cc56c0705d7495dbe06961876
thenerdie <stirmanjr@gmail.coom>
2021-05-31 23:14:08 -0400

    Make audio play on loop

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 const fs = require('fs');
 const path = require('path');
+const playSound = require('play-sound');
+const chalk = require('chalk');
 const getTTSWav = require('./getTTSWav')
 const Report = require('./report');
 
+const args = process.argv.slice(2)
+
+const audioPlayer = playSound()
+
 const weatherReport = new Report()
 
-weatherReport.getFullReport().then(async report => {
-    getTTSWav(report).then(fileContents => {
-        fs.writeFile(path.join(__dirname, 'report.wav'), fileContents, () => {})
+function doReport() {
+    weatherReport.getFullReport(args[0]).then(async report => {
+        console.log(chalk.yellow("Generated weather report! Getting TTS Base64 data..."))
+
+        getTTSWav(report).then(fileContents => {
+            console.log(chalk.magentaBright("Generated TTS Base64 data!"))
+            fs.writeFileSync(path.join(__dirname, 'report.wav'), fileContents)
+            console.log(chalk.green("Base64 written to wav file! Playing..."))
+
+            var process = audioPlayer.play("./report.wav")
+
+            process.on('exit', () => {
+                doReport()
+            })
+        })
     })
-})
+}
+
+doReport()
