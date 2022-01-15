2e0e5a10ee4892827f950d357fdfbaae60cef5aa
thenerdie <stirmanjr@gmail.com>
2022-01-15 00:40:48 -0500

    changes

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const handlebars = require("handlebars")
 const { format } = require("date-fns")
 const getDirection = require('degrees-to-direction');
 
+function parseTimestamp(time) {
+    return new Date(time * 1000)
+}
+
 handlebars.registerHelper("round", number => {
     return Math.floor(number + 0.5)
 })
@@ -41,25 +45,78 @@ handlebars.registerHelper("subtract", (a, b) => {
     return a - b
 })
 
+handlebars.registerHelper("multiply", (a, b) => {
+    return a * b
+})
+
 handlebars.registerHelper("convertCelcius", celcius => {
     return celcius * 9 / 5 + 32
 })
 
 handlebars.registerHelper("now", () => {
     const now = Date.now()
-    return format(now, "h m a 'on' MMMM d yyyy")
+    return format(now, "h:mm a 'on' MMMM d yyyy")
+})
+
+handlebars.registerHelper("timefull", (timestamp) => {
+    return format(parseTimestamp(timestamp), "h:mm a 'on' MMMM d yyyy")
+})
+
+handlebars.registerHelper("time", (timestamp) => {
+    return format(parseTimestamp(timestamp), "h:mm a")
+})
+
+handlebars.registerHelper("weekday", (timestamp) => {
+    return format(parseTimestamp(timestamp), "EEEE")
 })
 
 const fs = require("fs")
 
-const config = require("./config.json")
+const config = require("./config.json");
+const { setInterval } = require("timers/promises");
+const { off } = require("process");
 
-const templateString = fs.readFileSync("./reports/weather.hbs").toString()
-const template = handlebars.compile(templateString)
+const templateStringNow = fs.readFileSync("./reports/weather.hbs").toString()
+const templateNow = handlebars.compile(templateStringNow)
+
+const templateStringForecast = fs.readFileSync("./reports/forecast.hbs").toString()
+const templateForecast = handlebars.compile(templateStringForecast)
 
 let lastReport
 
 let attempts = 0
+
+let forecast
+
+let coordinates
+
+async function getCoordinates() {
+    const { data } = await axios.get("https://api.openweathermap.org/geo/1.0/direct", {
+        params: {
+            q: config.owm.city,
+            appid: config.owm.token
+        }
+    })
+
+    coordinates = {
+        lat: data[0].lat,
+        lon: data[0].lon
+    }
+}
+
+async function updateForecast() {
+    const { data } = await axios.get("https://api.openweathermap.org/data/2.5/onecall", {
+        params: {
+            ...coordinates,
+            appid: config.owm.token,
+            units: "imperial"
+        }
+    })
+
+    const { daily } = data
+
+    forecast = daily
+}
 
 setInterval(() => {
     attempts = 0
@@ -77,9 +134,11 @@ async function doReport() {
             }
         })
         
-        const report = template(data)
+        const report = `${templateNow(data)} ${templateForecast({ forecast: forecast })}`
+
+        console.log(report)
         
-        execSync(`echo \"${report}\" | festival --tts`)
+        // execSync(`echo \"${report}\" | festival --tts`)
         
         lastReport = report
     } catch(error) {
@@ -89,4 +148,11 @@ async function doReport() {
     }
 }
 
-doReport()
+async function main() {
+    await getCoordinates()
+    await updateForecast()
+
+    doReport()
+}
+
+main()
